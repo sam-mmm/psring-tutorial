90a4cadfd6a1c63e5e922291a95c66ad2d577306
sam <2740878+sam-mmm@users.noreply.github.com>
2023-11-29 10:16:55 +0000

    rateLimiter

--- order-service/src/main/java/io/github/sammmm/springtutorial/orderservice/Resilience4jController.java
+++ order-service/src/main/java/io/github/sammmm/springtutorial/orderservice/Resilience4jController.java
@@ -15,21 +15,6 @@ public class Resilience4jController {
         this.demoService = demoService;
     }
 
-    @GetMapping("/circuit-breaker")
-    public String circuitBreaker() {
-        return demoService.circuitBreaker();
-    }
-
-    @GetMapping("/bulkhead")
-    public String bulkhead() {
-        return demoService.bulkHead();
-    }
-
-    @GetMapping("/time-limiter")
-    public CompletableFuture<String> timeLimiter() {
-        return demoService.timeLimiter();
-    }
-
     @GetMapping("/rate-limiter")
     public String rateLimiter() {
         return demoService.rateLimiter();
@@ -40,5 +25,3 @@ public class Resilience4jController {
         return demoService.retry();
     }
 }
-//io.github.sammmm.springtutorial:payloads:jar:0.0.1
-//io/github/sammmm/springtutorial/payloads/0.0.1/payloads-0.0.1.jarcd 
